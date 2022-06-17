c3e99ab9615cc472966e26fb8050aef6de4b926f
user <user@DESKTOP-COJ9858>
2022-06-17 16:43:31 +0900

    1111

--- GitProject1/src/com/sist/MainClass.java
+++ GitProject1/src/com/sist/MainClass.java
@@ -5,10 +5,7 @@ public class MainClass {
 	public static void main(String[] args) {
 		
 		System.out.println("Hello Github!!");
-		System.out.println("Hello Github!!");
-		System.out.println("Hello Github!!");
-		System.out.println("Hello Github!!");
-		System.out.println("Hello Github!!");
+
 		// 형상 관리 ffhfhf
 	}
 }
